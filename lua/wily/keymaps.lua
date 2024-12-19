778bac197948d773822c9342aa2903d4c2309227
williamhCode <williamh.coding@gmail.com>
2024-12-19 17:06:50 +0800

    new neogurt session chooser

--- lua/wily/keymaps.lua
+++ lua/wily/keymaps.lua
@@ -61,8 +61,6 @@ else
 end
 
 -- tab
--- map("n", "<leader>te", "<cmd>tabe<CR>")
--- map("n", "<leader>tq", "<cmd>tabc<CR>")
 map("n", "<leader>]", "<cmd>tabn<CR>")
 map("n", "<leader>[", "<cmd>tabp<CR>")
 
@@ -265,14 +263,74 @@ map("n", "<leader>gg", "<Cmd>Neogit<CR>")
 if vim.g.neogurt then
   -- all modes
   local mode = {"", "!", "t", "l"};
+
+  map(mode, "<D-t>", "<cmd>tabnew<cr>")
+  map(mode, "<D-w>", "<cmd>tabclose<cr>")
+  map(mode, "<D-}>", "<cmd>tabnext<cr>")
+  map(mode, "<D-{>", "<cmd>tabprev<cr>")
+
   map(mode, "<D-=>", "<cmd>Neogurt font_size_change 1<cr>")
   map(mode, "<D-->", "<cmd>Neogurt font_size_change -1<cr>")
   map(mode, "<D-0>", "<cmd>Neogurt font_size_reset<cr>")
 
   map(mode, "<D-l>", "<cmd>Neogurt session_prev<cr>")
-  map(mode, "<D-r>", "<cmd>Neogurt session_select sort=time<cr>")
+  map(mode, "<D-R>", "<cmd>Neogurt session_restart<cr>")
 
   local choose_session = function(startup)
+    local curr_id = vim.g.neogurt_cmd("session_info").id
+    local session_list = vim.g.neogurt_cmd("session_list", { sort = "time" })
+
+    local cmd = [[
+    echo "$({
+      echo ~/;
+      echo ~/.dotfiles;
+      echo ~/.config/nvim; 
+      echo ~/Documents/Notes;
+      echo ~/Documents/Work/Resume stuff;
+      find ~/Documents/Coding -mindepth 2 -maxdepth 2 -type d | sort -r;
+    })"
+    ]]
+    local output = vim.fn.system(cmd)
+
+    for dir in string.gmatch(output, "([^\n]+)") do
+      table.insert(session_list, { dir = dir })
+    end
+
+    vim.ui.select(session_list, {
+      prompt = "Sessions",
+      format_item = function(session)
+        if session.id ~= nil then
+          if session.id == curr_id then
+            return "* " .. session.name
+          else
+            return "- " .. session.name
+          end
+        else
+          return session.dir
+        end
+      end
+    }, function(choice)
+      if choice == nil then return end
+
+      if choice.id ~= nil then
+        vim.g.neogurt_cmd("session_switch", { id = choice.id })
+      else
+        local fmod = vim.fn.fnamemodify
+        local dir = fmod(choice.dir, ":p")
+        local name = fmod(dir, ":h:h:t") .. "/" .. fmod(dir, ":h:t")
+
+        if startup then
+          local currId = vim.g.neogurt_cmd("session_info").id
+          vim.g.neogurt_cmd("session_new", { dir = dir, name = name })
+          vim.g.neogurt_cmd("session_kill", { id = currId })
+        else
+          vim.g.neogurt_cmd("session_new", { dir = dir, name = name })
+        end
+      end
+    end)
+  end
+
+  local new_session = function(startup)
     local cmd = [[
     echo "$({
       echo ~/;
@@ -291,7 +349,7 @@ if vim.g.neogurt then
     end
 
     vim.ui.select(dirs, {
-      prompt = "Choose a directory:",
+      prompt = "New Session",
     }, function(choice)
       if choice == nil then return end
       local dir = choice
@@ -307,11 +365,15 @@ if vim.g.neogurt then
     end)
   end
 
-  map(mode, "<D-f>", function()
+  map(mode, "<D-r>", function()
     choose_session(false)
   end)
 
+  map(mode, "<D-f>", function()
+    new_session(false)
+  end)
+
   vim.g.neogurt_startup = function()
-    choose_session(true)
+    new_session(true)
   end
 end
